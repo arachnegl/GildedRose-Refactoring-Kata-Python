c764d62e27765816c88cec7968fc0dde079a6c55
greg <1952671+arachnegl@users.noreply.github.com>
2021-02-26 14:31:39 +0100

    add new Product class framework

--- gilded_rose.py
+++ gilded_rose.py
@@ -8,37 +8,111 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+            self._update(item)
+
+    def _update(self, item):
+        if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+            if item.quality > 0:
+                if item.name != "Sulfuras, Hand of Ragnaros":
+                    item.quality = item.quality - 1
+        else:
+            if item.quality < 50:
+                item.quality = item.quality + 1
+                if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    if item.sell_in < 11:
+                        if item.quality < 50:
+                            item.quality = item.quality + 1
+                    if item.sell_in < 6:
+                        if item.quality < 50:
+                            item.quality = item.quality + 1
+        if item.name != "Sulfuras, Hand of Ragnaros":
+            item.sell_in = item.sell_in - 1
+        if item.sell_in < 0:
+            if item.name != "Aged Brie":
+                if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                    if item.quality > 0:
+                        if item.name != "Sulfuras, Hand of Ragnaros":
+                            item.quality = item.quality - 1
+                else:
+                    item.quality = item.quality - item.quality
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+
+
+class Item:
+    def __init__(self, name, sell_in, quality):
+        self.name = name
+        self.sell_in = sell_in
+        self.quality = quality
+
+    def __repr__(self):
+        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
 
 @dataclass
-class Item:
+class Product:
     name: str
     sell_in: int
     quality: int
+
+    max_quality: int = 50
+
+    _class_name_to_class = None
+
+    @classmethod
+    def _get_class(cls, item: Item):
+        if cls._class_name_to_class is None:
+            cls._class_name_to_class = {P.name: P for P in Product.__subclasses__()}
+        if item.name in cls._class_name_to_class:
+            return cls._class_name_to_class[item.name]
+        return cls
+
+    @classmethod
+    def from_item(cls, item):
+        """Dispatches an Item object to its specialised Product if found else to 
+        a generic Product.
+        """
+        return cls._get_class(item)(
+            name=item.name,
+            sell_in=item.sell_in,
+            quality=item.quality,
+        )
+
+    def to_item(self):
+        return Item(
+            name=self.name,
+            sell_in=self.sell_in,
+            quality=self.quality,
+        )
+
+    def update(self):
+        """The generic case
+        """
+        self.sell_in = self.sell_in - 1
+        return self
+
+
+class Sulfuras(Product):
+    name = "Sulfuras, Hand of Ragnaros"
+    quality = 80   # constant
+
+    max_quality: int = 80
+
+    def update(self) -> None:
+        """
+        Sulfuras, being a legendary item, never has to be sold or decreases in Quality
+        """
+        return self
+
+
+class AgedBrie(Product):
+    name = 'Aged Brie'
+
+    def update(self) -> None:
+        """
+        "Aged Brie" actually increases in Quality the older it gets
+        """
+        if self.quality < 50:
+            self.quality = self.quality + 1
+        return self
--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 import unittest
 
-from gilded_rose import Item, GildedRose
+from gilded_rose import (
+    Item, 
+    Product,
+    GildedRose,
+    Sulfuras,
+    AgedBrie,
+)
 
 
 _items = [
@@ -33,17 +39,64 @@ class GildedRoseTest(unittest.TestCase):
             self.assertEqual(orig['name'], updated.name)
         
 
-class ItemTest(unittest.TestCase):
+class ProductTest(unittest.TestCase):
 
     def test_equality(self):
+        # import pdb; pdb.set_trace()
         self.assertEqual(
-             Item(name="A", sell_in=2, quality=0),
-             Item(name="A", sell_in=2, quality=0),
+             Product(name="A", sell_in=2, quality=0),
+             Product(name="A", sell_in=2, quality=0),
         )
         self.assertNotEqual(
-             Item(name="A", sell_in=1, quality=0),
-             Item(name="A", sell_in=2, quality=0),
+             Product(name="A", sell_in=1, quality=0),
+             Product(name="A", sell_in=2, quality=0),
         )
+
+
+class ProductTest(unittest.TestCase):
+    item = Item(name="A", sell_in=0, quality=80)
+    klass = Product
+
+    def test_to_and_from_item(self):
+        as_product = Product.from_item(self.item)
+        self.assertEqual(type(as_product), self.klass)
+        self.assertEqual(
+            # as Item doesn't support element wise equality we fall back on 
+            # (somewhat brittle) repr comparison.
+            str(Product.to_item(as_product)),
+            str(self.item),
+        )
+
+
+class SulfurasTest(ProductTest):
+    item = Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80)
+    klass = Sulfuras
+
+    def test_update(self):
+        self.assertEqual(
+            Sulfuras(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80).update(),
+            Sulfuras(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+        )
+        self.assertEqual(
+            Sulfuras(name="Sulfuras, Hand of Ragnaros", sell_in=40, quality=80).update(),
+            Sulfuras(name="Sulfuras, Hand of Ragnaros", sell_in=40, quality=80),
+        )
+        self.assertNotEqual(
+            Sulfuras(name="Sulfuras, Hand of Ragnaros", sell_in=40, quality=80).update(),
+            Sulfuras(name="Sulfuras, Hand of Ragnaros", sell_in=39, quality=80),
+        )
+
+
+class AgedBrieTest(ProductTest):
+    item = Item('Aged Brie', 10, 10)
+    klass = AgedBrie
+
+    def test_update(self):
+        self.assertEqual(
+            AgedBrie(name="Aged Brie", sell_in=10, quality=10).update(),
+            AgedBrie(name="Aged Brie", sell_in=10, quality=11),
+        )
+
 
 if __name__ == '__main__':
     unittest.main()
